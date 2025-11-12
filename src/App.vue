8aac59e68ae99b04733be2dc3c4083f55b8c8290
Bilic Sara <sara.bilic@student.howest.be>
2025-11-12 10:48:39 +0100

    Add base functionality of Mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <Map/>
+  <!-- Top bar which displays coordinates & reset button-->
+  <div id="sidebar">
+    Longitude: {{ location.center.lng.toFixed(4) }} | Latitude:
+    {{ location.center.lat.toFixed(4) }} | Zoom:
+    {{ location.zoom.toFixed(2) }} |
+    <button
+      @click="
+        location = {
+          center: { lng: 4.4235414 , lat: 51.2163428 },
+          zoom: 14,
+        }
+      "
+    >
+      Reset
+    </button>
+    <!--The button variable "location" has to be changed too!!!-->
+
+  </div>
+  <Map v-model="location" />
 </template>
 
 <script>
@@ -7,8 +25,30 @@ import Map from "./components/Map.vue";
 
 export default {
   components: { Map },
+
+  //Initial location => change for HTF!!!! 
+  data() {
+    return {
+      location: {
+        center: { lng: 4.4235414 , lat: 51.2163428 },
+        zoom: 14,
+      },
+    };
+  },
 };
 </script>
 
 <style>
+#sidebar {
+  background-color: rgb(35 55 75 / 90%);
+  color: #fff;
+  padding: 6px 12px;
+  font-family: monospace;
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 12px;
+  border-radius: 4px;
+}
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,15 +7,31 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 
-//CHANGE THIS TO THE GIVEN API KEY!!!
-mapboxgl.accessToken = "APIKEY";
+// API access token for Mapbox
+mapboxgl.accessToken =
+  "";
 
 export default {
+  props: ["modelValue"],
+
   mounted() {
+    const { center, zoom } = this.modelValue
+
+    // instantiate the map using the center and zoom from the modelValue prop
     const map = new mapboxgl.Map({
       container: this.$refs.mapContainer,
       style: "mapbox://styles/mapbox/standard",
+      center: center,
+      zoom,
     });
+
+    // function to update the modelValue prop with the map's current location
+    const updateLocation = () =>
+      this.$emit("update:modelValue", this.getLocation());
+
+    // add event listeners to update the location on map move and zoom
+    map.on("move", updateLocation);
+    map.on("zoom", updateLocation);
 
     // assign the map instance to this component's map property
     this.map = map;
@@ -25,7 +41,35 @@ export default {
   unmounted() {
     this.map.remove();
     this.map = null;
-  }
+  },
+  
+  // watch for external changes to the modelValue prop and update the map accordingly
+  watch: {
+    modelValue(next) {
+      const curr = this.getLocation();
+
+      // Only flyTo if any of the values have changed
+      if (
+        curr.center.lng !== next.center.lng ||
+        curr.center.lat !== next.center.lat ||
+        curr.zoom !== next.zoom
+      ) {
+        this.map.flyTo({
+          center: next.center,
+          zoom: next.zoom,
+        });
+      }
+    },
+  },
+  
+  methods: {
+    getLocation() {
+      return {
+        center: this.map.getCenter(),
+        zoom: this.map.getZoom(),
+      };
+    },
+  },
 };
 </script>
 
