371b2cbf9658077811870a962a656e940781f405
Bilic Sara <sara.bilic@student.howest.be>
2025-11-12 10:37:50 +0100

    Mapbox baseline

---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+
+//CHANGE THIS TO THE GIVEN API KEY!!!
+mapboxgl.accessToken = "APIKEY";
+
+export default {
+  mounted() {
+    const map = new mapboxgl.Map({
+      container: this.$refs.mapContainer,
+      style: "mapbox://styles/mapbox/standard",
+    });
+
+    // assign the map instance to this component's map property
+    this.map = map;
+  },
+
+  // clean up the map instance when the component is unmounted
+  unmounted() {
+    this.map.remove();
+    this.map = null;
+  }
+};
+</script>
+
+<style>
+/* make the map container fill its parent */
+.map-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
